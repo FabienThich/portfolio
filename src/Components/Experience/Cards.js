de7dfdc07f0f07a12c39540de821ad88c04227bc
FabienThich <fabienthich30@gmail.com>
2023-01-06 16:14:13 -0500

    Update Cards Info

--- src/Components/Experience/Cards.js
+++ src/Components/Experience/Cards.js
@@ -1,30 +1,32 @@
 import PersonalWebsite from "../../assets/Personal Website.png";
+import Calculator from "../../assets/Calculator.png";
 import JPCountries from "../../assets/JPCountries.png";
 
 const Cards = [
   {
-    title: "Personal Website",
+    title: "Fabien. T",
     status: "In Progress",
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "Personal website that documents my experience at North P&D as well as my education history.",
     image: PersonalWebsite,
     link: "https://fabienthich.github.io/",
   },
 
   {
-    title: "Weather App",
+    title: "Budget Planner",
     status: "Coming Soon",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image: JPCountries,
     link: "https://www.jpabadir.com/countries",
   },
+
   {
     title: "Calculator",
     status: "",
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image: JPCountries,
+      "A simple calculator built with React.js as a learning project.",
+    image: Calculator,
     link: "https://fabienthich.github.io/calculator",
   },
 
@@ -32,10 +34,11 @@ const Cards = [
     title: "SVG Lag",
     status: "",
     description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "Implemented a solution to resolve a delayed loading for a Scalable Vector Graphic of a map.",
     image: JPCountries,
     link: "https://www.jpabadir.com/countries",
   },
+  
   {
     title: "SVG Lag",
     status: "",
