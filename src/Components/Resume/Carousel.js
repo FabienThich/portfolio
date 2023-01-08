24955a48af1fe8fface28838fc1bf0f26ae0e254
FabienThich <fabienthich30@gmail.com>
2023-01-08 12:35:48 -0500

    Test

---
+++ src/Components/Resume/Carousel.js
@@ -0,0 +1,36 @@
+import ReactLogo from "../../assets/React.svg";
+import BootstrapLogo from "../../assets/Bootstrap.svg";
+import PythonLogo from "../../assets/Python.svg";
+import NodeJSLogo from "../../assets/NodeJS.svg";
+
+const CarouselData = [
+  {
+    title: "School Project",
+    caption:
+      "Personal website that documents my experience at North P&D as well as my education history.",
+    image: ReactLogo,
+  },
+
+  {
+    title: "School Project",
+    caption:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: BootstrapLogo,
+  },
+
+  {
+    title: "Certification",
+    caption:
+      "Personal website that documents my experience at North P&D as well as my education history.",
+    image: ReactLogo,
+  },
+
+  {
+    title: "Certification",
+    caption:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image: BootstrapLogo,
+  },
+];
+
+export default CarouselData;
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,11 +1,35 @@
 import "./Resume.css";
+import Carousel from "react-bootstrap/Carousel";
+import CarouselData from "./Carousel.js";
 
-function Resume() {
+function Academics() {
   return (
-    <div className="container Pad">
-      <h1>Resume</h1>
+    <div className="Pad border">
+      <div className="container border">
+        <h1>Academics</h1>
+      </div>
+      <Carousel className="CarouselParent">
+        {CarouselData.map((data) => {
+          return (
+            <Carousel.Item>
+              <div className="row">
+                <div className="col-md-6 col-sm-12 text-center border">
+                  <img
+                    className="carouselImage rounded-5 border"
+                    src={data.image}
+                  />
+                </div>
+                <div className="col-md-6 col-sm-12 carouselDesc border">
+                  <h3>{data.title}</h3>
+                  <p>{data.caption} </p>
+                </div>
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
 
-export default Resume;
+export default Academics;
