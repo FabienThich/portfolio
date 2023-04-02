df516afa1c563dcb8a96a903d2018d630733e730
FabienThich <fabienthich30@gmail.com>
2023-04-02 15:52:14 -0400

    Add Offset to Animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { React, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import AOS from 'aos'
+import AOS from "aos";
 import "aos/dist/aos.css";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
