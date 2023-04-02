4832e95495aa3e57c0a8c3b08cd7665b71a4fc11
FabienThich <fabienthich30@gmail.com>
2023-04-02 15:06:28 -0400

    Add animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import { React, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import AOS from 'aos'
+import "aos/dist/aos.css";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 700, once: "True" });
+  }, []);
   return (
     <div className="App">
       <Main />
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,10 +7,10 @@ import NodeJSLogo from "../../assets/NodeJS.svg";
 function About() {
   return (
     <div className="container PadExempt">
-      <h1>About Me</h1>
+      <h1 data-aos="fade-up">About Me</h1>
       <div className="aboutParent">
         <div className="col-md-6 col-sm-12 aboutDesc">
-          <div style={{ fontSize: "20px" }}>
+          <div data-aos="fade-up" style={{ fontSize: "20px" }}>
             I am a specialist high skills major student working as a junior
             software engineer for North P&D, Inc. I specialize in React.js,
             Bootstrap and Python, though I am familiarizing myself with Node.js.
@@ -19,13 +19,32 @@ function About() {
             and cooking.
           </div>
           <div className="d-flex justify-content-between">
-            <img className="stackLogo" src={ReactLogo} />
-            <img className="stackLogo" src={BootstrapLogo} />
-            <img className="stackLogo" src={PythonLogo} />
-            <img className="stackLogo" src={NodeJSLogo} />
+            <img
+              data-aos="zoom-in-up"
+              className="stackLogo"
+              src={ReactLogo}
+            />
+            <img
+              data-aos="zoom-in-up"
+              data-aos-delay="50"
+              className="stackLogo"
+              src={BootstrapLogo}
+            />
+            <img
+              data-aos="zoom-in-up"
+              data-aos-delay="100"
+              className="stackLogo"
+              src={PythonLogo}
+            />
+            <img
+              data-aos="zoom-in-up"
+              data-aos-delay="150"
+              className="stackLogo"
+              src={NodeJSLogo}
+            />
           </div>
         </div>
-        <div className="col-md-6 col-sm-12 aboutImage">
+        <div data-aos="fade-up" className="col-md-6 col-sm-12 aboutImage">
           <img
             className="myHeadshot"
             src="https://sdk.bitmoji.com/render/panel/a41da708-81e9-4ec3-9364-9d8748f7063d-0c814c04-ec17-4cf9-adea-101958aaede1-v1.png?transparent=1&palette=1"
