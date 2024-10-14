edb2bc5013c1a042fe3694e33699206162c7c266
FabienThich <fabienthich30@gmail.com>
2024-10-14 13:04:26 -0400

    Add hover effect + work on status

--- src/Components/AcademicWork/AcademicWork.js
+++ src/Components/AcademicWork/AcademicWork.js
@@ -8,13 +8,13 @@ function AcademicWork() {
       <div className="row">
         {Work.map((card) => {
           return (
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-5">
+            <div className="col-lg-4 col-md-6 col-sm-12 mb-5 cardHover">
               <div data-aos="fade-up" className="card work-card">
                 <img src={card.image} className="work-card-image" />
                 <div className="card-body">
                   <h5 className="card-title">
                     {card.title}
-                    <p className="blogStatus">{card.readLength}</p>
+                    <p className={card.readLength <= 5 ? "blogShortRead" : 'blogLongRead'}>{card.readLength} Minute Read</p>
                   </h5>
                   <p className="card-text">{card.description}</p>
                   <a className="btn btn-dark rounded-pill seeBlog">Read Blog</a>
--- src/Components/AcademicWork/Work.js
+++ src/Components/AcademicWork/Work.js
@@ -3,7 +3,7 @@ const Work = [
     title: "Spotify",
     image:
       "https://images.squarespace-cdn.com/content/v1/6042529f1fe38f0b0503be5c/1616072527567-YHOUT1L2JW6ROBTME3SQ/spotify-playlist.jpeg",
-    readLength: "10 Minutes",
+    readLength: 10,
     description: "Desc",
     blog: "Blog",
   },
@@ -12,7 +12,7 @@ const Work = [
     title: "Airbnb",
     image:
       "https://blog.effectivetours.com/wp-content/uploads/2016/07/Airbnb-new-logo-2014.png",
-    readLength: "5 Minutes",
+    readLength: 5,
     description: "Desc",
     blog: "Blog",
   },
@@ -21,7 +21,7 @@ const Work = [
     title: "Overwatch",
     image:
       "https://wallpapers.com/images/featured/overwatch-dpacmg5qk3abi7qn.jpg",
-    readLength: "5 Minutes",
+    readLength: 5,
     description: "Desc",
     blog: "Blog",
   },
