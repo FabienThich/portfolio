e1a65d1d898accf77902332e75309838f8a653d6
FabienThich <fabienthich30@gmail.com>
2024-10-15 10:37:03 -0400

    Update contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -95,6 +95,25 @@ function Contact() {
                   required
                 />
               </div>
+
+              <div
+                className="d-flex justify-content-center align-items-center mb-2"
+                data-aos="fade-up"
+                data-aos-delay="150"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-envelope-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+                </svg>
+                <p className="mt-3 ms-2 contactEmail">fabien06@my.yorku.ca</p>
+              </div>
+
               <div
                 data-aos="fade-up"
                 data-aos-delay="200"
